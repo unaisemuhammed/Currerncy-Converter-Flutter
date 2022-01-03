09d13855697e441275a61097878949594ac4d092
Mohammed Unaise <unys313@gmail.com>
2022-01-03 18:59:32 +0530

    completed

--- lib/View/HomeSceen/button.dart
+++ lib/View/HomeSceen/button.dart
@@ -20,11 +20,37 @@ class _ButtonContainerState extends State<ButtonContainer> {
       children: [
         GestureDetector(
           onTap: () async {
+             FocusScope.of(context).unfocus();
             //validating amount field
             amountController.customTextFieldValidation();
-            amountController.exchangeCurrency();
-            FocusScope.of(context).unfocus();
+            //functionCalling
+            var result = await exchangeRateCurrency.exchangeRateCurrencies(
+                dropDownController.value.toString(),
+                dropDownController.value2.toString());
+
+            //decodingToJson
+            var conversionData = jsonDecode(result!);
+
+            //gettingTargetCurrencyValue
+            totalController.targetCurrencyValue =
+                conversionData[dropDownController.value2?.toLowerCase()].toString();
+
+            //gettingTotalValue
+            totalController.totalValue = (double.parse(
+                amountController.amount.text == ''
+                    ? '1'
+                    : amountController.amount.text) *
+                double.parse(totalController.targetCurrencyValue))
+                .toString();
+            String history =
+                "${amountController.amount.text.toString() == '' ? '1' : amountController.amount.text.toString()}   ${dropDownController.value}    =    ${totalController.totalValue.toString()}   ${dropDownController.value2!}";
+
+            //AddToDatabase
+            dataBaseController.addCurrencyHistories(
+                historyValue: history, dateTime: conversionData['date'].toString());
+
             setState(() {});
+
           },
           child: Container(
             padding: const EdgeInsets.all(15.0),
